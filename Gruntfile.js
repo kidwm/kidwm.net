2a061b17dbcc031622a92db297eb3fa1a356ad54
WM Chang <wandererm@gmail.com>
2013-07-30 17:31:36 +0800

    add links

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,37 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-	  sass: {
-		dist: {
-		  files: {
-			'style.css': 'style.scss'
-		  }
-		}
-	  },
-	csslint: {
-		strict: {
-			options: {
-				'known-properties': false,
-				'box-model': false
-			},
-			src: ['style.css']
-		}
-	}
-	});
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-csslint');
-	grunt.registerTask('default', ['sass', 'csslint']);
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'style.css': 'style.scss'
+                }
+            }
+        },
+        csslint: {
+            strict: {
+                options: {
+                    'known-properties': false,
+                    'box-model': false
+                },
+                src: ['style.css']
+            }
+        },
+        watch: {
+            livereload: {
+                files: ['*.scss'],
+                tasks: ['sass'],
+                options: {
+                    livereload: true
+                },
+            },
+        },
+        connect: {
+            uses_defaults: {}
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('default', ['connect', 'watch']);
 };
